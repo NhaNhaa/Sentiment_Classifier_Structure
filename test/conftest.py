b87ee523b7bc576184c26255a542fbf01ebc74bb
NhaNhaa <bonajfjj@gmail.com>
2026-04-08 17:29:34 +0700

    chore: remove obsolete training and test files

--- test/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest configuration - adds parent directory to Python path."""
-
-import sys
-import os
-
-# Add the parent directory (project root) to Python path
-# This allows 'import config' to work in test files
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
